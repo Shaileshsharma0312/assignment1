17caef21f0902a70a1aee58675a5c6502705ba43
shaileshsharma0312 <shaileshsharma0312@gmail.com>
2016-06-22 13:23:53 +0530

    Changes To add Sulfras new

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -12,9 +12,14 @@ class GildedRose {
             if (!items[i].name.equals("Aged Brie")
                     && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
+                	if(!items[i].name.equals("Sulfuras New")){
+                		if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                			items[i].quality = items[i].quality - 1;
+                		}
+                	} else
+                	{
+                		items[i].quality= items[i].quality > 10 ? items[i].quality - 1 : 10;
+                	}
                 }
             } else {
                 if (items[i].quality < 50) {
@@ -43,12 +48,16 @@ class GildedRose {
             if (items[i].sellIn < 0) {
                 if (!items[i].name.equals("Aged Brie")) {
                     if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                     if(!items[i].name.equals("Sulfuras New")){
                         if (items[i].quality > 0) {
                             if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
                                 items[i].quality = items[i].quality - 1;
                             }
                         }
                     } else {
+                    	items[i].quality= items[i].quality > 10 ? items[i].quality - 1 : 10;
+                    }
+                    }else {
                         items[i].quality = items[i].quality - items[i].quality;
                     }
                 } else {
